f41f0765415d8ba42d5e7817537e2fff0f38e2fa
Dwi Fuady <dwifuady@gmail.com>
2026-07-16 20:55:56 +0700

    fix: widen search bar max-w to match GlobalSearch internal constraint

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -94,7 +94,7 @@ export function TitleBar({ sidebarWidth = 280, sidebarHidden = false, onToggleSi
         </div>
 
         {/* Search Bar - aligned with content flow */}
-        <div className="ml-auto mr-2 pointer-events-auto shrink-0 max-w-[300px]">
+        <div className="ml-auto mr-2 pointer-events-auto shrink-0 max-w-md">
           <GlobalSearch />
         </div>
       </div>
